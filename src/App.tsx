aa65b06e69ab9f0ea72241cf18acb2e2f08f5428
Waqas Munir <devwaqas232@gmail.com>
2025-02-24 21:26:12 +0500

    Setup Shadcn Ui

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Button } from "./components/ui/button";
+
+export default function App(){
+  return <div>
+    Hello World
+    <Button variant={"destructive"}>Shadcn Button</Button>
+    </div>
+}
